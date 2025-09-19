87a709410806cece4e1d72e8f88feac715e49be5
Vaibhav Pawar <vaibhavhpawar5@gmail.com>
2025-09-19 18:38:19 +0530

    create sperate folder for backed

---
+++ backend/src/main/java/com/example/sweet_shop/controller/SweetController.java
@@ -0,0 +1,96 @@
+package com.example.sweetshop.controller;
+
+import com.example.sweetshop.model.Sweet;
+import com.example.sweetshop.service.SweetService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/sweets")
+public class SweetController {
+
+    private final SweetService sweetService;
+
+    public SweetController(SweetService sweetService) {
+        this.sweetService = sweetService;
+    }
+
+    // POST /api/sweets → add new sweet
+    @PostMapping
+    public ResponseEntity<Sweet> addSweet(@RequestBody Sweet sweet) {
+        return ResponseEntity.ok(sweetService.addSweet(sweet));
+    }
+
+    // GET /api/sweets → list all
+    @GetMapping
+    public ResponseEntity<List<Sweet>> getAllSweets() {
+        return ResponseEntity.ok(sweetService.getAllSweets());
+    }
+
+    // GET /api/sweets/{id} → get by id
+    @GetMapping("/{id}")
+    public ResponseEntity<Sweet> getSweetById(@PathVariable String id) {
+        return sweetService.getSweetById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // PUT /api/sweets/{id} → update
+    @PutMapping("/{id}")
+    public ResponseEntity<Sweet> updateSweet(@PathVariable String id, @RequestBody Sweet sweet) {
+        return ResponseEntity.ok(sweetService.updateSweet(id, sweet));
+    }
+
+    // DELETE /api/sweets/{id} → delete
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSweet(@PathVariable String id) {
+        sweetService.deleteSweet(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    // GET /api/sweets/category/{category}
+    @GetMapping("/category/{category}")
+    public ResponseEntity<List<Sweet>> getSweetsByCategory(@PathVariable String category) {
+    return ResponseEntity.ok(sweetService.searchByCategory(category));
+    }
+
+    // GET /api/sweets/search?name=lado&minPrice=10&maxPrice=50
+    @GetMapping("/search")
+    public ResponseEntity<List<Sweet>> searchSweets(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String category,
+            @RequestParam(required = false) Double minPrice,
+            @RequestParam(required = false) Double maxPrice) {
+            
+        if((name != null) && (category != null) && (minPrice != null) && (maxPrice != null))
+        {
+            return ResponseEntity.ok(sweetService.searchSweets(name, category, minPrice, maxPrice));
+        }
+        else if (name != null) {
+            return ResponseEntity.ok(sweetService.searchByName(name));
+        } else if (minPrice != null && maxPrice != null) {
+            return ResponseEntity.ok(sweetService.searchByPriceRange(minPrice, maxPrice));
+        } 
+        else if (category != null) {
+            return ResponseEntity.ok(sweetService.searchByCategory(category));
+        }else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PostMapping("/{id}/purchase")
+    public Sweet purchaseSweet(@PathVariable String id, @RequestParam int qunt) {
+    return sweetService.purchaseSweet(id,qunt);
+    }
+
+    @PostMapping("/{id}/restock")
+    public ResponseEntity<Sweet> restockSweet(
+        @PathVariable String id,
+        @RequestParam int quantity) {
+    Sweet updatedSweet = sweetService.restockSweet(id, quantity);
+    return ResponseEntity.ok(updatedSweet);
+}
+
+}
---
+++ backend/src/main/java/com/example/sweet_shop/model/Sweet.java
@@ -0,0 +1,37 @@
+package com.example.sweetshop.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "sweets") // MongoDB collection name
+public class Sweet {
+    @Id
+private String id;          // Unique ID for MongoDB
+    private String name;        // Name of the sweet
+    private String category;    // Category (e.g., Indian, Chocolate, etc.)
+    private double price;       // Price per unit
+    private int quantity;
+
+    // --- Getters & Setters ---
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+
+    public int getQuantity() { return quantity; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+}
